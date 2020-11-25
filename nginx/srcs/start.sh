24a010fa13477c84d9b4d4991e0eaa2c2339a3c7
Masato Iwaura <miwaura@c1r9s5.42tokyo.jp>
2020-11-25 13:59:49 +0900

    php

--- nginx/srcs/start.sh
+++ nginx/srcs/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rc-status
 rc-service nginx start
+rc-update add vsftpd default
+rc-service vsftpd restart
 rc-service php-fpm7 start
 tail -f /dev/null
--- srcs/mysql/srcs/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rc-status
-rc-update add mariadb
-/etc/init.d/mariadb restart
-tail -f /dev/null
